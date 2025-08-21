4770c969de478cfaf32020f2789a20d94411c600
odevpedro <pedrosschmidt2@gmail.com>
2025-08-21 15:32:59 -0300

    fix: ajustes integração dos modulos

--- deck-service/src/main/java/com/odevpedro/yugiohcollections/deck/DeckServiceApplication.java
+++ deck-service/src/main/java/com/odevpedro/yugiohcollections/deck/DeckServiceApplication.java
@@ -1,11 +1,13 @@
 package com.odevpedro.yugiohcollections.deck;
 
+import com.odevpedro.yugiohcollections.deck.adapter.out.cardcatalog.CardCatalogClient;
+import com.odevpedro.yugiohcollections.deck.adapter.out.external.CardFeignClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "com.odevpedro.yugiohcollections.deck.adapter.out.cardcatalog")
+@EnableFeignClients(basePackageClasses = { CardFeignClient.class, CardCatalogClient.class })
 public class DeckServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(DeckServiceApplication.class, args);
--- deck-service/src/main/java/com/odevpedro/yugiohcollections/deck/adapter/out/external/CardFeignClient.java
+++ deck-service/src/main/java/com/odevpedro/yugiohcollections/deck/adapter/out/external/CardFeignClient.java
@@ -2,13 +2,13 @@ package com.odevpedro.yugiohcollections.deck.adapter.out.external;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @FeignClient(name = "card-service", url = "http://localhost:8080")
 public interface CardFeignClient {
 
-    @GetMapping("/cards/by-ids")
-    List<CardResponseDTO> findCardsByIds(@RequestParam("ids") List<Long> ids);
+    @GetMapping("/internal/cards/{id}")
+    List<CardResponseDTO> findCardsByIds(@PathVariable("id") List<Long> ids);
 }
