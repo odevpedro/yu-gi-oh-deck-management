e25baad01b5e86d522a3e71eca707ac6234869a3
odevpedro <pedrosschmidt2@gmail.com>
2026-03-03 21:40:19 -0300

    adicionando auth no card-creator-service

--- card-creator-service/src/main/java/com/odevpedro/yugiohcollections/creator/CardCreatorApplication.java
+++ card-creator-service/src/main/java/com/odevpedro/yugiohcollections/creator/CardCreatorApplication.java
@@ -4,9 +4,14 @@ import com.odevpedro.yugiohcollections.shared.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableConfigurationProperties(JwtProperties.class)
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.odevpedro.yugiohcollections.creator",
+        "com.odevpedro.yugiohcollections.shared"
+})
 public class CardCreatorApplication {
     public static void main(String[] args) {
         SpringApplication.run(CardCreatorApplication.class, args);
